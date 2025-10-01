vChewing/vChewing-macOS

Repo // Patch logger API for macOS 26.

Installer/MainViewImpl.swift
@@ -9,6 +9,7 @@
 import AppKit
 import IMKUtils
 import InputMethodKit
+import SwiftExtension
 
 extension MainView {
   public func removeThenInstallInputMethod() {
@@ -27,11 +28,11 @@ extension MainView {
         // 塞入垃圾桶
         try fileManager.trashItem(at: fileURL, resultingItemURL: nil)
       } else {
-        NSLog("File does not exist")
+        String.consoleLog("File does not exist")
       }
 
     } catch let error as NSError {
-      NSLog("An error took place: \(error)")
+      String.consoleLog("An error took place: \(error)")
     }
 
     let killTask = Process()
@@ -88,7 +89,8 @@ extension MainView {
     let imeBundleURL = theBundle.bundleURL
 
     if allRegisteredInstancesOfThisInputMethod.isEmpty {
-      NSLog("Registering input source \(imeIdentifier) at \(imeBundleURL.absoluteString).")
+      String
+        .consoleLog("Registering input source \(imeIdentifier) at \(imeBundleURL.absoluteString).")
       let status = (TISRegisterInputSource(imeBundleURL as CFURL) == noErr)
       if !status {
         isShowingAlertForMissingPostInstall = true
@@ -102,7 +104,7 @@ extension MainView {
           ) + "(#D41J0U8U)",
           imeIdentifier
         )
-        NSLog(message)
+        String.consoleLog(message)
       }
     }
 
@@ -118,9 +120,9 @@ extension MainView {
       if isActivated { return }
       // WARNING: macOS 12 may return false positives, hence forced activation.
       if neta.activate() {
-        NSLog("Input method enabled: \(imeIdentifier)")
+        String.consoleLog("Input method enabled: \(imeIdentifier)")
       } else {
-        NSLog("Failed to enable input method: \(imeIdentifier)")
+        String.consoleLog("Failed to enable input method: \(imeIdentifier)")
       }
     }
 

Packages/vChewing_LangModelAssembly/Sources/LangModelAssembly/vChewingLMAssembly_Common.swift
@@ -8,6 +8,7 @@
 
 import CSQLite3
 import Foundation
+import SwiftExtension
 
 // MARK: - LMAssembly
 
@@ -99,11 +100,8 @@ func performStatementSansResult(_ handler: (inout OpaquePointer?) -> ()) {
 }
 
 func vCLMLog(_ strPrint: StringLiteralType) {
-  guard let toLog = UserDefaults.standard.object(forKey: "_DebugMode") as? Bool else {
-    NSLog("vChewingDebug: %@", strPrint)
-    return
-  }
+  let toLog = UserDefaults.standard.object(forKey: "_DebugMode") as? Bool ?? true
   if toLog {
-    NSLog("vChewingDebug: %@", strPrint)
+    String.consoleLog("vChewingDebug: \(strPrint)")
   }
 }

Packages/vChewing_OSFrameworkImpl/Sources/OSFrameworkImpl/AppKitImpl/AppKitImpl_Misc.swift
@@ -21,7 +21,7 @@ extension Bundle {
       &code
     )
     guard status4Code == 0, let code = code else {
-      NSLog("Error from getCodeSignedDate(): Failed from retrieving status4Code.")
+      String.consoleLog("Error from getCodeSignedDate(): Failed from retrieving status4Code.")
       return nil
     }
     let status = SecCodeCopySigningInformation(
@@ -30,16 +30,16 @@ extension Bundle {
       &information
     )
     guard status == noErr else {
-      NSLog("Error from getCodeSignedDate(): Failed from retrieving code signing intelligence.")
+      String.consoleLog("Error from getCodeSignedDate(): Failed from retrieving code signing intelligence.")
       return nil
     }
     guard let dictionary = information as? [String: NSObject] else { return nil }
     guard dictionary[kSecCodeInfoIdentifier as String] != nil else {
-      NSLog("Error from getCodeSignedDate(): Target not signed.")
+      String.consoleLog("Error from getCodeSignedDate(): Target not signed.")
       return nil
     }
     guard let infoDate = dictionary[kSecCodeInfoTimestamp as String] as? Date else {
-      NSLog("Error from getCodeSignedDate(): Target signing timestamp is missing.")
+      String.consoleLog("Error from getCodeSignedDate(): Target signing timestamp is missing.")
       return nil
     }
     return infoDate as Date
@@ -233,10 +233,10 @@ extension FileManager {
           at: URL(fileURLWithPath: path), resultingItemURL: nil
         )
       } else {
-        NSLog("Item doesn't exist: \(path)")
+        String.consoleLog("Item doesn't exist: \(path)")
       }
     } catch let error as NSError {
-      NSLog("Failed from removing this object: \(path) || Error: \(error)")
+      String.consoleLog("Failed from removing this object: \(path) || Error: \(error)")
       return false
     }
     return true

Packages/vChewing_Uninstaller/Sources/Uninstaller/Uninstaller.swift
@@ -26,7 +26,7 @@ public enum Uninstaller {
     )
     // 輸入法自毀處理。這裡不用「Bundle.main.bundleURL」是為了方便使用者以 sudo 身分來移除被錯誤安裝到系統目錄內的輸入法。
     guard let bundleID = Bundle.main.bundleIdentifier else {
-      NSLog("Failed to ensure the bundle identifier.")
+      String.consoleLog("Failed to ensure the bundle identifier.")
       return -1
     }
 

Packages/vChewing_UpdateSputnik/Sources/UpdateSputnik/UpdateSputnik.swift
@@ -97,7 +97,7 @@ public class UpdateSputnik {
       return
     }
 
-    NSLog("update check plist: \(plist)")
+    String.consoleLog("update check plist: \(plist)")
 
     guard let intRemoteVersion = Int(plist[kCFBundleVersionKey] as? String ?? ""),
           let strRemoteVersionShortened = plist["CFBundleShortVersionString"] as? String
@@ -224,7 +224,7 @@ public class UpdateSputnik {
   }
 
   private func showError(message: String = "") {
-    NSLog("Update check: plist error, forced check: \(isCurrentCheckForced)")
+    String.consoleLog("Update check: plist error, forced check: \(isCurrentCheckForced)")
     if !isCurrentCheckForced { return }
     let alert = NSAlert()
     let content = message

Source/Modules/main.swift
@@ -12,6 +12,7 @@ import InputMethodKit
 import LangModelAssembly
 import MainAssembly
 import Shared
+import SwiftExtension
 import Uninstaller
 
 let cmdParameters = CommandLine.arguments.dropFirst(1)
@@ -84,13 +85,13 @@ default: exit(0)
 }
 
 guard let mainNibName = Bundle.main.infoDictionary?["NSMainNibFile"] as? String else {
-  NSLog("vChewingDebug: Fatal error: NSMainNibFile key not defined in Info.plist.")
+  String.consoleLog("vChewingDebug: Fatal error: NSMainNibFile key not defined in Info.plist.")
   exit(-1)
 }
 
 let loaded = Bundle.main.loadNibNamed(mainNibName, owner: NSApp, topLevelObjects: nil)
 if !loaded {
-  NSLog("vChewingDebug: Fatal error: Cannot load \(mainNibName).")
+  String.consoleLog("vChewingDebug: Fatal error: Cannot load \(mainNibName).")
   exit(-1)
 }
 
@@ -101,7 +102,7 @@ let kConnectionName = Bundle.main
 guard let bundleID = Bundle.main.bundleIdentifier,
       let server = IMKServer(name: kConnectionName, bundleIdentifier: bundleID)
 else {
-  NSLog(
+  String.consoleLog(
     "vChewingDebug: Fatal error: Cannot initialize input method server with connection name retrieved from the plist, nor there's no connection name in the plist."
   )
   exit(-1)

retro1996/vchewing

Installer/MainViewImpl.swift
@@ -9,6 +9,7 @@
 import AppKit
 import IMKUtils
 import InputMethodKit
+import SwiftExtension
 
 extension MainView {
   public func removeThenInstallInputMethod() {
@@ -27,11 +28,11 @@ extension MainView {
         // 塞入垃圾桶
         try fileManager.trashItem(at: fileURL, resultingItemURL: nil)
       } else {
-        NSLog("File does not exist")
+        Process.consoleLog("File does not exist")
       }
 
     } catch let error as NSError {
-      NSLog("An error took place: \(error)")
+      Process.consoleLog("An error took place: \(error)")
     }
 
     let killTask = Process()
@@ -88,7 +89,9 @@ extension MainView {
     let imeBundleURL = theBundle.bundleURL
 
     if allRegisteredInstancesOfThisInputMethod.isEmpty {
-      NSLog("Registering input source \(imeIdentifier) at \(imeBundleURL.absoluteString).")
+      Process.consoleLog(
+        "Registering input source \(imeIdentifier) at \(imeBundleURL.absoluteString)."
+      )
       let status = (TISRegisterInputSource(imeBundleURL as CFURL) == noErr)
       if !status {
         isShowingAlertForMissingPostInstall = true
@@ -102,7 +105,7 @@ extension MainView {
           ) + "(#D41J0U8U)",
           imeIdentifier
         )
-        NSLog(message)
+        Process.consoleLog(message)
       }
     }
 
@@ -118,9 +121,9 @@ extension MainView {
       if isActivated { return }
       // WARNING: macOS 12 may return false positives, hence forced activation.
       if neta.activate() {
-        NSLog("Input method enabled: \(imeIdentifier)")
+        Process.consoleLog("Input method enabled: \(imeIdentifier)")
       } else {
-        NSLog("Failed to enable input method: \(imeIdentifier)")
+        Process.consoleLog("Failed to enable input method: \(imeIdentifier)")
       }
     }
 

Packages/vChewing_LangModelAssembly/Sources/LangModelAssembly/vChewingLMAssembly_Common.swift
@@ -8,6 +8,7 @@
 
 import CSQLite3
 import Foundation
+import SwiftExtension
 
 // MARK: - LMAssembly
 
@@ -99,11 +100,8 @@ func performStatementSansResult(_ handler: (inout OpaquePointer?) -> ()) {
 }
 
 func vCLMLog(_ strPrint: StringLiteralType) {
-  guard let toLog = UserDefaults.standard.object(forKey: "_DebugMode") as? Bool else {
-    NSLog("vChewingDebug: %@", strPrint)
-    return
-  }
+  let toLog = UserDefaults.standard.object(forKey: "_DebugMode") as? Bool ?? true
   if toLog {
-    NSLog("vChewingDebug: %@", strPrint)
+    Process.consoleLog("vChewingDebug: \(strPrint)")
   }
 }

Packages/vChewing_OSFrameworkImpl/Sources/OSFrameworkImpl/AppKitImpl/AppKitImpl_Misc.swift
@@ -21,7 +21,7 @@ extension Bundle {
       &code
     )
     guard status4Code == 0, let code = code else {
-      NSLog("Error from getCodeSignedDate(): Failed from retrieving status4Code.")
+      Process.consoleLog("Error from getCodeSignedDate(): Failed from retrieving status4Code.")
       return nil
     }
     let status = SecCodeCopySigningInformation(
@@ -30,16 +30,16 @@ extension Bundle {
       &information
     )
     guard status == noErr else {
-      NSLog("Error from getCodeSignedDate(): Failed from retrieving code signing intelligence.")
+      Process.consoleLog("Error from getCodeSignedDate(): Failed from retrieving code signing intelligence.")
       return nil
     }
     guard let dictionary = information as? [String: NSObject] else { return nil }
     guard dictionary[kSecCodeInfoIdentifier as String] != nil else {
-      NSLog("Error from getCodeSignedDate(): Target not signed.")
+      Process.consoleLog("Error from getCodeSignedDate(): Target not signed.")
       return nil
     }
     guard let infoDate = dictionary[kSecCodeInfoTimestamp as String] as? Date else {
-      NSLog("Error from getCodeSignedDate(): Target signing timestamp is missing.")
+      Process.consoleLog("Error from getCodeSignedDate(): Target signing timestamp is missing.")
       return nil
     }
     return infoDate as Date
@@ -233,10 +233,10 @@ extension FileManager {
           at: URL(fileURLWithPath: path), resultingItemURL: nil
         )
       } else {
-        NSLog("Item doesn't exist: \(path)")
+        Process.consoleLog("Item doesn't exist: \(path)")
       }
     } catch let error as NSError {
-      NSLog("Failed from removing this object: \(path) || Error: \(error)")
+      Process.consoleLog("Failed from removing this object: \(path) || Error: \(error)")
       return false
     }
     return true

Packages/vChewing_Uninstaller/Sources/Uninstaller/Uninstaller.swift
@@ -26,7 +26,7 @@ public enum Uninstaller {
     )
     // 輸入法自毀處理。這裡不用「Bundle.main.bundleURL」是為了方便使用者以 sudo 身分來移除被錯誤安裝到系統目錄內的輸入法。
     guard let bundleID = Bundle.main.bundleIdentifier else {
-      NSLog("Failed to ensure the bundle identifier.")
+      Process.consoleLog("Failed to ensure the bundle identifier.")
       return -1
     }
 

Packages/vChewing_UpdateSputnik/Sources/UpdateSputnik/UpdateSputnik.swift
@@ -97,7 +97,7 @@ public class UpdateSputnik {
       return
     }
 
-    NSLog("update check plist: \(plist)")
+    Process.consoleLog("update check plist: \(plist)")
 
     guard let intRemoteVersion = Int(plist[kCFBundleVersionKey] as? String ?? ""),
           let strRemoteVersionShortened = plist["CFBundleShortVersionString"] as? String
@@ -224,7 +224,7 @@ public class UpdateSputnik {
   }
 
   private func showError(message: String = "") {
-    NSLog("Update check: plist error, forced check: \(isCurrentCheckForced)")
+    Process.consoleLog("Update check: plist error, forced check: \(isCurrentCheckForced)")
     if !isCurrentCheckForced { return }
     let alert = NSAlert()
     let content = message

Source/Modules/main.swift
@@ -12,6 +12,7 @@ import InputMethodKit
 import LangModelAssembly
 import MainAssembly
 import Shared
+import SwiftExtension
 import Uninstaller
 
 let cmdParameters = CommandLine.arguments.dropFirst(1)
@@ -84,13 +85,13 @@ default: exit(0)
 }
 
 guard let mainNibName = Bundle.main.infoDictionary?["NSMainNibFile"] as? String else {
-  NSLog("vChewingDebug: Fatal error: NSMainNibFile key not defined in Info.plist.")
+  Process.consoleLog("vChewingDebug: Fatal error: NSMainNibFile key not defined in Info.plist.")
   exit(-1)
 }
 
 let loaded = Bundle.main.loadNibNamed(mainNibName, owner: NSApp, topLevelObjects: nil)
 if !loaded {
-  NSLog("vChewingDebug: Fatal error: Cannot load \(mainNibName).")
+  Process.consoleLog("vChewingDebug: Fatal error: Cannot load \(mainNibName).")
   exit(-1)
 }
 
@@ -101,7 +102,7 @@ let kConnectionName = Bundle.main
 guard let bundleID = Bundle.main.bundleIdentifier,
       let server = IMKServer(name: kConnectionName, bundleIdentifier: bundleID)
 else {
-  NSLog(
+  Process.consoleLog(
     "vChewingDebug: Fatal error: Cannot initialize input method server with connection name retrieved from the plist, nor there's no connection name in the plist."
   )
   exit(-1)